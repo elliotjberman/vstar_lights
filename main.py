82663a88454c004b00e7b77f36e0a8ab1b0f0863
Elliot Berman <elliotjberman@gmail.com>
2018-05-14 17:39:43 +0000

    basic http server working

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 #!env/bin/python3.5
 
-import socket, time, atexit
-import opc, animations, renderer
-import keyboard
+import urllib, threading
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import animations, renderer
 
+PORT_NUMBER = 8000
 
+class MidiHandler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = urllib.parse.parse_qs(self.rfile.read(content_length).decode())
+        print(post_data)
+        if post_data['channel'][0] == "0":
+            triangle = animations.FullFlash(colour="white", frames=5)
+        else:
+            triangle = animations.FullFlash(colour="mint", frames=10)
+
+        triangle_1.add_layer(triangle)
+        self.send_response(200)
+
+server = HTTPServer(('0.0.0.0', PORT_NUMBER), MidiHandler)
 triangle_1 = renderer.Renderer()
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind(('127.0.0.1', 2346))
-
-print("Starting animation loop")
-while True:
-    message, address = server_socket.recvfrom(1024)
-    print(message)
-    if keyboard.is_pressed('space'):
-        triangle = animations.FullFlash(colour="peach", frames=60)
-        triangle_1.add_layer(triangle)
-        
-    if keyboard.is_pressed('enter'):
-        triangle = animations.FullFlash(colour="mint", frames=10)
-        triangle_1.add_layer(triangle)
-
+def animate():
     triangle_1.render()
-    triangle_2.render()
+    threading.Timer(1/60, animate).start()
 
-    time.sleep(1/60) # 60 FPS
+animate()
+server.serve_forever()
