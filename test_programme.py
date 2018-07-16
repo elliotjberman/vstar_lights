efb51d19f87b7e086696a2ea0d427019647d30cb
Elliot Berman <elliotjberman@gmail.com>
2018-07-16 18:53:42 -0400

    cleaned up test programme

--- test_programme.py
+++ test_programme.py
@@ -8,24 +8,24 @@ for _ in range(5):
     triangles.append(renderer.Renderer())
 
 client = opc.Client('localhost:7890')
-
-# Main
-atexit.register(lambda: animations.clear(client))
-
 parser = argparse.ArgumentParser(description='Light All Triangles with colour from colour store')
-parser.add_argument('-c', '--colour', type=str, default='white', nargs='?',
-                    help='Colour to test')
-parser.add_argument('-p', '--programme', type=str, default='FullFlash', nargs='?',
-                    help='Programme to test')
-parser.add_argument('-f', '--freeze', action='store_true',
-                    help='Freeze first frame of animation (good for checking colours)')
-parser.set_defaults(freeze=False)
 
-args = parser.parse_args()
-print("Testing Colour '{}' with Programme '{}'. Exit program to turn off triangles".format(args.colour, args.programme))
-# End Main
+def main():
+    atexit.register(lambda: animations.clear_all(client))
 
-def animate():
+    parser.add_argument('-c', '--colour', type=str, default='white', nargs='?',
+                        help='Colour to test')
+    parser.add_argument('-p', '--programme', type=str, default='FullFlash', nargs='?',
+                        help='Programme to test')
+    parser.add_argument('-f', '--freeze', action='store_true',
+                        help='Freeze first frame of animation (good for checking colours)')
+    parser.set_defaults(freeze=False)
+
+    args = parser.parse_args()
+    print("Testing Colour '{}' with Programme '{}'. Exit program to turn off triangles".format(args.colour, args.programme))
+    animate_with_args(args)
+
+def animate_with_args(args):
     leds = []
     for triangle in triangles:
         if not triangle.layers:
@@ -37,6 +37,6 @@ def animate():
     client.put_pixels(leds) 
     while args.freeze:
         client.put_pixels(leds) 
-    threading.Timer(1/60, animate).start()
+    threading.Timer(1/60, lambda: animate_with_args(args)).start()
 
-animate()
+main()
